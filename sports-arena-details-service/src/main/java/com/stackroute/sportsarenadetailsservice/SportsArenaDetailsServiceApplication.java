a245172960b725fbcca6d899a695e811a6bb773c
chaitali <chaitali.chatterjee@stackroute.in>
2023-07-31 10:51:40 +0530

    Initial commit

---
+++ sports-arena-details-service/src/main/java/com/stackroute/sportsarenadetailsservice/SportsArenaDetailsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.sportsarenadetailsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsArenaDetailsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsArenaDetailsServiceApplication.class, args);
+	}
+
+}
---
+++ sports-arena-details-service/src/test/java/com/stackroute/sportsarenadetailsservice/SportsArenaDetailsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.sportsarenadetailsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsArenaDetailsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
