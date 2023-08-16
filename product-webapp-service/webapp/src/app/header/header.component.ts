8578658a5b00d0bea647a696641ef3e2d698ee9c
aashishhh01 <aashishhh01@gmail.com>
2023-08-16 10:04:49 +0530

    profile update connected to backend

--- product-webapp-service/webapp/src/app/header/header.component.ts
+++ product-webapp-service/webapp/src/app/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
 import { MatDialog } from '@angular/material/dialog';
 import { SignupComponent } from '../signup/signup.component';
+import { ProfileUpdateComponent } from '../profile-update/profile-update.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,12 +11,14 @@ import { SignupComponent } from '../signup/signup.component';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  constructor(private _dialog : MatDialog){}
+  constructor(private _dialog : MatDialog,
+    private _router: Router
+    ){}
   openLoginForm(){
     this._dialog.open(LoginComponent)
   }
   openRegisterationForm(){
     this._dialog.open(SignupComponent)
   }
-
+ 
 }
