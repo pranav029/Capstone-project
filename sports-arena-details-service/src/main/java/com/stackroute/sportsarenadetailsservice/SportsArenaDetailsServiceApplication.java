b8243f32f77f684fca1705c0c32964136ff5a2a1
pranav029 <pranavmani.naman@gmail.com>
2023-08-17 16:05:33 +0530

    Make detail service eureka client

--- sports-arena-details-service/src/main/java/com/stackroute/sportsarenadetailsservice/SportsArenaDetailsServiceApplication.java
+++ sports-arena-details-service/src/main/java/com/stackroute/sportsarenadetailsservice/SportsArenaDetailsServiceApplication.java
@@ -2,8 +2,10 @@ package com.stackroute.sportsarenadetailsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SportsArenaDetailsServiceApplication {
 
 	public static void main(String[] args) {
