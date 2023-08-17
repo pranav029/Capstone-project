cac22740b32ebe9cd11a376ecbd5a4f47afaabc0
pranav029 <pranavmani.naman@gmail.com>
2023-08-17 15:56:27 +0530

    Update imports

--- sports-arena-details-service/src/main/java/com/stackroute/sportsarenadetailsservice/controller/ArenaDetailController.java
+++ sports-arena-details-service/src/main/java/com/stackroute/sportsarenadetailsservice/controller/ArenaDetailController.java
@@ -4,14 +4,13 @@ import com.stackroute.sportsarenadetailsservice.dto.request.GroundDto;
 import com.stackroute.sportsarenadetailsservice.dto.request.ResponseDto;
 import com.stackroute.sportsarenadetailsservice.services.ArenaDetail.ArenaDetailService;
 import com.stackroute.sportsarenadetailsservice.services.Image.ImageUploadService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
