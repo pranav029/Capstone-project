6af5220dbbd46cf94bff76c8ec0977d50c3f2fdc
pranav029 <pranavmani.naman@gmail.com>
2023-08-18 16:14:52 +0530

    Tweak controllers to handle ambiguity

--- user-service/src/main/java/com/stackroute/userservice/Controller/UserController.java
+++ user-service/src/main/java/com/stackroute/userservice/Controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/register")
+@RequestMapping("/api/v1/thrive/user")
 
 public class UserController {
     @Autowired
