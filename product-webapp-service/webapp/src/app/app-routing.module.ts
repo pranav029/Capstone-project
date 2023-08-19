705b604ae1d98288300ef48623b9620401c36425
pranav029 <pranavmani.naman@gmail.com>
2023-08-20 01:15:14 +0530

    Add authorization guard & code cleanup

--- product-webapp-service/webapp/src/app/app-routing.module.ts
+++ product-webapp-service/webapp/src/app/app-routing.module.ts
@@ -12,22 +12,22 @@ import { HomeComponent } from './components/home/home.component';
 import { ProfileUpdateComponent } from './profile-update/profile-update.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
-import { AuthGuard } from './services/auth-guard.guard';
+import { AuthGuard, AuthorizationGuard, LoginGuard } from './services/auth-guard.guard';
 
 
 
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
+  { path: 'signup', component: SignupComponent, canActivate: [LoginGuard] },
   { path: "", redirectTo: "/login", pathMatch: "full" },
-  { path: 'form', component: ArenaDetailsFormComponent, canActivate: [AuthGuard] },
-  { path: 'ownerBookings', component: OwnerbookingsComponent, canActivate: [AuthGuard] },
+  { path: 'form', component: ArenaDetailsFormComponent, canActivate: [AuthGuard, AuthorizationGuard] },
+  { path: 'ownerBookings', component: OwnerbookingsComponent, canActivate: [AuthGuard, AuthorizationGuard] },
   { path: 'playerBookings', component: BookingdetailsComponent, canActivate: [AuthGuard] },
   { path: 'saveSlots', component: SavingslotComponent, canActivate: [AuthGuard] },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'profile', component: ProfileUpdateComponent, canActivate: [AuthGuard] },
-  { path: 'dashboard', component: DashboardComponent}
+  { path: 'dashboard', component: DashboardComponent }
 
 
 ];
