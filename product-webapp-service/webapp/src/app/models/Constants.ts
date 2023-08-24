9823598b70fa019fc5794c8a0ce2ef02e5baa792
chaitali <chaitali.chatterjee@stackroute.in>
2023-08-24 19:31:16 +0530

    static folder recreated

--- product-webapp-service/webapp/src/app/models/Constants.ts
+++ product-webapp-service/webapp/src/app/models/Constants.ts
@@ -1,2 +1,2 @@
 // export const THRIVE_BASE_URL = "http://localhost:8090"
-export const THRIVE_BASE_URL="https://thrivesportsarena.stackroute.io/";
+export const THRIVE_BASE_URL="https://thrivesportsarena.stackroute.io";
